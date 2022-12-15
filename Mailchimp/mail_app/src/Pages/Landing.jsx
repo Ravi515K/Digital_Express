3e5c9d23f4f47ecbad233fb4f63bf4f415e1e5c9
Ravi515K <koliravi95@gmail.com>
2022-12-15 23:39:13 +0530

    day-3work

--- Mailchimp/mail_app/src/Pages/Landing.jsx
+++ Mailchimp/mail_app/src/Pages/Landing.jsx
@@ -1,6 +1,7 @@
-import { Box, Button, Stack, Image,Text, Container, Heading } from "@chakra-ui/react";
+import { Box, Button, Stack, Image,Text, Container, Heading, VStack, ListItem, List,ListIcon,Center } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { AspectRatio } from '@chakra-ui/react'
+import {CheckIcon, CloseIcon}  from "@chakra-ui/icons";
 
 export default function Landing(){
     return (
@@ -92,12 +93,14 @@ export default function Landing(){
                 <Box >
                      <AspectRatio  ml="300px" mt="20px" maxW='760px' ratio={1}>
                         <iframe
+                          autoPlay
                             title='naruto'
                             src='https://www.youtube.com/embed/QhBnZ6NPOY0'
-                            allowFullScreen
+                            
                         
                         />
                     </AspectRatio>
+                   
                     <Container ml="300px" color="white"  maxW='2xl' centerContent>
                         <Box fontSize={"20px"}>
                             Most email marketing platforms tell you how your campaigns perform. We analyze the data from the billions of 
@@ -108,10 +111,244 @@ export default function Landing(){
                 </Box>
             </Box>
 
-            <Box boxSize={"100%"} p="100px">
+            <Box className="Plan" boxSize={"100%"} p="100px" bg="grey.400">
                 <Heading>Find the right plan</Heading>
-                
+                <Box height={"auto"} mt="50px">
+                    <Stack direction="horizontal">
+                        <Box boxShadow={"2xl"} width={"350px"} border={"1px solid grey"} padding="10px" >
+                            <Text textAlign={"left"} fontSize={"25px"}>Premium</Text>
+                            <Container >
+                                <Box textAlign={"left"} color="grey.200">
+                                     Our most advanced tools, unlimited contacts, and priority support; built for teams.
+                                </Box>
+                                <Box textAlign={"left"} position="relative" mt="5px">
+                                    <Text>Starts at</Text>
+                                    <Text fontSize={"15px"} as="span" position="absolute" top="25px" left="2px">₹</Text>
+                                    <Heading ml="11px">23,000</Heading>
+                                    <Text color="grey">/month based on 10,000 contacts*</Text>
+                                </Box>
+                            </Container>
+                            <VStack>
+                                <Box px={6} py={10} fontSize="15px">
+                                    <List spacing={5}>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                           <Text borderBottom="1px dotted black">Phone & Priority Support</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                          <Text borderBottom="1px dotted black" > Custom-Coded and Pre-built Email Templates</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Multivariate and A/B Testing</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                           <Text borderBottom="1px dotted black">Enhanced Automated Customer Journeys</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                          <Text  borderBottom="1px dotted black" > Predictive Segmentation</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Assisted Onboarding</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                    </List>
+                                </Box>
+                            </VStack>
+                        </Box>
+                        
+                        <Box boxShadow={"2xl"} width={"350px"} border={"1px solid grey"} padding="10px" >
+                            <Text textAlign={"left"} fontSize={"25px"}>Standards</Text>
+                            <Container >
+                                <Box textAlign={"left"} color="grey.200">
+                                Sell even more with personalization, optimization tools, and enhanced automations.
+                                </Box>
+                                <Box textAlign={"left"} position="relative" mt="5px">
+                                    <Text>Starts at</Text>
+                                    <Text fontSize={"15px"} as="span" position="absolute" top="25px" left="2px">₹</Text>
+                                    <Heading ml="11px">1,150</Heading>
+                                    <Text color="grey">/month based on 10,000 contacts*</Text>
+                                </Box>
+                            </Container>
+                            <VStack>
+                                <Box px={6} py={10} fontSize="15px">
+                                    <List spacing={5}>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                           <Text borderBottom="1px dotted black">24/7 Email & Chat Support</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                          <Text borderBottom="1px dotted black" > Custom-Coded and Pre-built Email Templates</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Custom-Coded and Pre-built Email Templates</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                           <Text borderBottom="1px dotted black">Enhanced Automated Customer Journeys</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                          <Text  borderBottom="1px dotted black" > Predictive Segmentation</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CloseIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Assisted Onboarding</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                    </List>
+                                </Box>
+                            </VStack>
+                        </Box>
+
+                        <Box boxShadow={"2xl"} width={"350px"} border={"1px solid grey"} padding="10px" >
+                            <Text textAlign={"left"} fontSize={"25px"}>Essential</Text>
+                            <Container >
+                                <Box textAlign={"left"} color="grey.200">
+                                Send the right content at the right time with testing and scheduling features.
+                                </Box>
+                                <Box textAlign={"left"} position="relative" mt="5px">
+                                    <Text>Starts at</Text>
+                                    <Text fontSize={"15px"} as="span" position="absolute" top="25px" left="2px">₹</Text>
+                                    <Heading ml="11px">770</Heading>
+                                    <Text color="grey">/month based on 10,000 contacts*</Text>
+                                </Box>
+                            </Container>
+                            <VStack>
+                                <Box px={6} py={10} fontSize="15px">
+                                    <List spacing={5}>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                           <Text borderBottom="1px dotted black">24/7 Email & Chat Support</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                          <Text borderBottom="1px dotted black" >Pre-built Email Templates</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Basic Automated Customer Journeys</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                           <Text borderBottom="1px dotted black">A/B Testing</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CloseIcon} mr="15px" />
+                                          <Text  borderBottom="1px dotted black" > Predictive Segmentation</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex" >
+                                            <ListIcon as={CloseIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Assisted Onboarding</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                    </List>
+                                </Box>
+                            </VStack>
+                        </Box>
+                        
+                        
+                        <Box boxShadow={"xl"} width={"350px"} border={"1px solid grey"} padding="10px" >
+                            <Text textAlign={"left"} fontSize={"25px"}>Free</Text>
+                            <Container >
+                                <Box textAlign={"left"} color="grey.200">
+                                Send the right content at the right time with testing and scheduling features.
+                                </Box>
+                                <Box textAlign={"left"} position="relative" mt="5px">
+                                    <Text>Starts at</Text>
+                                    <Text fontSize={"15px"} as="span" position="absolute" top="25px" left="2px">₹</Text>
+                                    <Heading ml="11px">0</Heading>
+                                    <Text color="grey">/month based on 10,000 contacts*</Text>
+                                </Box>
+                            </Container>
+                            <VStack>
+                                <Box px={6} py={10} fontSize="15px">
+                                    <List spacing={5}>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px"/>
+                                           <Text borderBottom="1px dotted black">Email support for first 30 days</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CheckIcon} mr="15px" />
+                                          <Text borderBottom="1px dotted black" >Pre-built Email Templates</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CloseIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Basic Automated Customer Journeys</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CloseIcon} mr="15px" />
+                                           <Text borderBottom="1px dotted black">A/B Testing</Text> 
+                                        </ListItem>
+                                        <hr />
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CloseIcon} mr="15px" />
+                                          <Text  borderBottom="1px dotted black" > Predictive Segmentation</Text>  
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                        <ListItem textAlign={"left"} display="flex">
+                                            <ListIcon as={CloseIcon} mr="15px"/>
+                                            <Text borderBottom="1px dotted black" >Assisted Onboarding</Text>
+                                            
+                                        </ListItem>
+                                        <hr/>
+                                    </List>
+                                </Box>
+                            </VStack>
+                        </Box>
+
+                        
+
+
+                    </Stack>
+                    <Center mt="50px">
+                        <Link href="">
+                             <Button w="200px" h="50px" borderRadius={"50px"} bg="gold">See Plan Details</Button>
+                        </Link>
+                        
+                    </Center>
+                </Box>
             </Box>
+             
         </div>
     )
 }
